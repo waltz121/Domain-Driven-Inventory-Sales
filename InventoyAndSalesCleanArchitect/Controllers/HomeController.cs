42f4ccd81b745e7bd86c18375b440c06f8670715
Walter R. Robledo <walter.r.robledo@compare88.net>
2019-08-23 17:51:40 +0800

    Removed Application Layer Temporarily

--- InventoyAndSalesCleanArchitect/Controllers/HomeController.cs
+++ InventoyAndSalesCleanArchitect/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
 ﻿using InventorySales.Application.Sales.Queries.GetSalesQueries;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace InventoyAndSalesCleanArchitect.Controllers

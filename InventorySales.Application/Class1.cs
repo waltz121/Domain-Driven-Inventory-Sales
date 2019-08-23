1c5ca65a1fa25eddfa3226ce15af7133729131a2
Walter R. Robledo <walter.r.robledo@compare88.net>
2019-08-23 17:52:52 +0800

    Added Initial Inventory Sales Application

---
+++ InventorySales.Application/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySales.Application
+{
+    public class Class1
+    {
+    }
+}

0c75cf7ad7ae8a9908ccafe7ed595d2ad82ebd89
Walter R. Robledo <walter.r.robledo@compare88.net>
2019-08-30 13:03:25 +0800

    Added Product Folder and other Components for product

---
+++ InventoyAndSalesCleanArchitect/Product/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoyAndSalesCleanArchitect.Product
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

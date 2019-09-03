6d667d37dbc4d77a7cd68627f27fb7b0fc6335cc
Walter R. Robledo <walter.r.robledo@compare88.net>
2019-09-03 17:56:06 +0800

    Added Additional codes for ViewModel Factory

--- InventoyAndSalesCleanArchitect/Product/Model/CreateProductViewModel.cs
+++ InventoyAndSalesCleanArchitect/Product/Model/CreateProductViewModel.cs
@@ -3,11 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace InventoyAndSalesCleanArchitect.Product.Model
 {
     public class CreateProductViewModel
     {
+        public List<SelectListItem> productTypesList { get; set; }
         public CreateProductModel createProductModel { get; set; }
+         
     }
 }
--- InventoyAndSalesCleanArchitect/Product/Services/CreateProductViewModelFactory.cs
+++ InventoyAndSalesCleanArchitect/Product/Services/CreateProductViewModelFactory.cs
@@ -1,15 +1,32 @@
 ﻿using System;
+using System.Linq;
+using System.Web.Mvc;
 using InventorySales.Application.Products.Commands.CreateProduct;
+using InventorySales.Application.Products.Queries.GetProductTypesList;
 using InventoyAndSalesCleanArchitect.Product.Model;
 
 namespace InventoyAndSalesCleanArchitect.Product.Services
 {
     public class CreateProductViewModelFactory : ICreateProductViewModelFactory
     {
+        private readonly IGetProductTypesListQuery getProductTypesListQuery;
+        public CreateProductViewModelFactory(IGetProductTypesListQuery getProductTypesListQuery)
+        {
+            this.getProductTypesListQuery = getProductTypesListQuery;
+        }
         public CreateProductViewModel Create()
         {
             CreateProductViewModel ViewModel = new CreateProductViewModel();
             ViewModel.createProductModel = new CreateProductModel();
+
+            var ProductTypesList = getProductTypesListQuery.Execute();
+
+            ViewModel.productTypesList = ProductTypesList.Select(p => new SelectListItem()
+            {
+                Value = p.ProductTypeCode.ToString(),
+                Text = p.ProductTypeDescription.ToString()
+            })
+            .ToList();
 
             return ViewModel;
         }

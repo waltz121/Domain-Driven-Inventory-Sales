c08d6c733370a0f8ac8f73def630b4941ab2aab0
Walter R. Robledo <robleo.walterrobbin@gmail.com>
2019-11-25 13:52:29 +0800

    Added Product Image Table for Image Storing of Products

---
+++ InventorySales.SQLDB/dbo/tables/Product_Image.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Product_Image]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProductId] INT NOT NULL, 
+    [Image] VARBINARY(MAX) NOT NULL, 
+    [Description] NVARCHAR(90) NULL, 
+    CONSTRAINT [FK_Product_Image_ToProduct] FOREIGN KEY ([ProductId]) REFERENCES [Products]([Id]) 
+)
